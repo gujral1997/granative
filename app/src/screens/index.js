dddb11557ce897e0a507f556c73e10eadb8957e3
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-04 16:16:56 +0530

    initiated routing and created blank screen

---
+++ app/src/screens/index.js
@@ -0,0 +1,5 @@
+import LoginScreen from './loginScreen/index'
+
+export {
+    LoginScreen
+}
---
+++ app/src/screens/loginScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+class LoginScreen extends React.Component {
+    render() {
+        return(
+            <View/>
+        )
+    }
+}
+
+export default LoginScreen
